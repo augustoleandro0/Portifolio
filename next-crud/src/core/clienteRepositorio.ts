2ec11cacfcd53a390fe2a957d84b42bac820d3f8
Pedro Leandro <>
2021-11-28 18:32:22 -0300

    remoção do next crud

--- next-crud/src/core/clienteRepositorio.ts
+++
@@ -1,7 +0,0 @@
-import Cliente from "./Cliente";
-
-export default interface ClienteRepositorio {
-    save(cliente: Cliente) : Promise<Cliente>
-    delete(cliente: Cliente) : Promise<void>
-    all() : Promise<Cliente[]>
-} 
